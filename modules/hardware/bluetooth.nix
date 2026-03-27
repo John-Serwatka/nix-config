53008d290d64449ac4f4308143c4984872829c1d
John Serwatka <johnchrisserwatka@gmail.com>
2026-03-27 11:09:31 -0400

    fixed bluetooth and added programs

--- modules/hardware/bluetooth.nix
+++ modules/hardware/bluetooth.nix
@@ -2,6 +2,18 @@
 { ... }:
 
 {
-  hardware.bluetooth.enable = true;
-  services.blueman.enable   = true;
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+
+    settings = {
+      General = {
+        Experimental = true;
+        FastConnectable = true;
+      };
+    };
+  };
+
+  # disable blueman for now
+  services.blueman.enable = false;
 }
